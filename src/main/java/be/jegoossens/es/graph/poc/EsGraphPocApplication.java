c0b5ac68544b8397a003965cec538a4b87775f36
jegoossens <jensgoossens@telenet.be>
2017-01-15 00:10:13 +0100

    Initial commit

---
+++ src/main/java/be/jegoossens/es/graph/poc/EsGraphPocApplication.java
@@ -0,0 +1,12 @@
+package be.jegoossens.es.graph.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsGraphPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsGraphPocApplication.class, args);
+	}
+}
---
+++ src/test/java/be/jegoossens/es/graph/poc/EsGraphPocApplicationTests.java
@@ -0,0 +1,16 @@
+package be.jegoossens.es.graph.poc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EsGraphPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
